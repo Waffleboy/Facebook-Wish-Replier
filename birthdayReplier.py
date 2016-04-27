387707f7d01465064cf5b37bfffccf7c5b1fc49e
waffleboy <A0130737@nus.edu.sg>
2016-04-27 14:11:58 +0800

    added save and silent functions

--- birthdayReplier.py
+++ birthdayReplier.py
@@ -7,11 +7,12 @@ Created on Mon Apr 25 15:42:30 2016
 from facepy import GraphAPI
 import random as rand
 from dateutil import parser
+import csv
 
 ##Insert Token from GraphAPI here
 TOKEN = 'INSERT TOKEN HERE'
 #Keywords to look for
-hitWords = set(['hb','birthday','bday','bday!','happy'])
+hitWords = set(['hb','birthday','bday','bday!','happy','hbd','birfday'])
 #Replies to give
 randomReply = ['Thanks! :)','Thank you! :)','Thanks alot! :)']
 
@@ -34,21 +35,30 @@ def postReply(postID):
     #like post
     graph.post(postID+'/likes')
     #comment post
-    graph.post(postID+'/comments',message= rand.choice(randomReply))
+    graph.post(postID+'/comments', message = rand.choice(randomReply))
 
 """ end subsidary functions """
 
 
 ### MAIN FUNCTION ###
-def main():
+
+#Input: 
+#1) <Boolean> Silent :if true, individual names of posters will not be printed to console.
+#2) <Boolean> save :if true, will save people who wished you to csv file for reference.
+def main(silent=True,save=False):
     global TOKEN
-    
+    counter = 0 
     graph = GraphAPI(TOKEN) #initialize facepy
     currentPage=graph.get('me/feed') # Get my latest posts
     # Get my Information
     myInfo = graph.get("me")
     myBirthdate = parser.parse(myInfo['birthday']).date()
     myID = myInfo['id']
+    
+    if save: #if save, create file to save to
+        with open('birthday'+str(myBirthdate.year)+'.csv','a') as f:
+            writer = csv.writer(f)
+            writer.writerow(['ID','Name'])
 
     nextPage = True
     while nextPage:
@@ -59,7 +69,8 @@ def main():
             date = parser.parse(entry['created_time']).date()
             if date.month < myBirthdate.month or date.day < myBirthdate.day:
                 nextPage = False
-                return
+                print('Completed. Replied '+str(counter)+' posts.')
+                return 
             
             # Skipping Conditions #
             #1 if poster is myself, skip
@@ -68,10 +79,20 @@ def main():
             
             #Else, check if birthday msg, post random reply
             if checkIfBirthdayMessage(entry['message']) == True:
+                counter+=1
                 postReply(entry['id'])
+                
+                if not silent:
+                    print('Replied '+entry['from']['name'])
+                if save:
+                    ID = entry['from']['id']
+                    name = entry['from']['name']
+                    with open('birthday'+str(myBirthdate.year)+'.csv','a') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([ID,name])
         #page
         currentPage = currentPage['paging']['next']
         currentPage = nextPage.replace('https://graph.facebook.com/v2.0/','')
 
 if __name__ == '__main__':
-    main()
+    main(silent=True,save=False)
